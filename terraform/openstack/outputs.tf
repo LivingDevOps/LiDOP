62c0225d9a81071e5e37691149e19810b3e15005
pascal.lauener <pascal.lauener@contractors.roche.com>
2019-04-10 14:14:38 +0200

    add outputs for openstack

--- terraform/openstack/outputs.tf
+++ terraform/openstack/outputs.tf
@@ -1,13 +1,15 @@
-# output "worker_public_ips" {
-#   value = "${aws_instance.worker.*.public_ip}"
-# }
-# output "master_public_ip" {
-#   value = "${aws_instance.master.*.public_ip}"
-# }
-# output "worker_private_ips" {
-#   value = "${aws_instance.worker.*.private_ip}"
-# }
-# output "master_private_ip" {
-#   value = "${aws_instance.master.*.private_ip}"
-# }
+output "worker_public_ips" {
+  value = "${openstack_networking_floatingip_v2.floating_ip_worker.*.address}"
+}
 
+output "master_public_ip" {
+  value = "${openstack_networking_floatingip_v2.floating_ip_master.*.address}"
+}
+
+output "worker_private_ips" {
+  value = "${openstack_compute_instance_v2.worker.*.network.0.fixed_ip_v4}"
+}
+
+output "master_private_ip" {
+  value = "${openstack_compute_instance_v2.master.*.network.0.fixed_ip_v4}"
+}
